542959d62dc545846a1774a615a54baac601a78e
Rodrigo M F Castilho <rodrigo.m.castilho@outlook.com>
2023-01-22 18:49:46 -0300

    Add modal

--- src/pages/index/components/LandingButtons.styles.js
+++ src/pages/index/components/LandingButtons.styles.js
@@ -1,11 +1,11 @@
-  import styled from 'styled-components';
+import styled from 'styled-components';
 
 
 export const LandingButtonWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
+  width: 100%;
   margin: 0 auto;
-  width: fit-content;
-  border: 1px solid red;
+  text-align: center;
 `
