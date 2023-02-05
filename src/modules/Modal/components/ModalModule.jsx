2c51af69722c76c1e51178e5ef9e9feff6c83bba
Rodrigo M F Castilho <rodrigo.m.castilho@outlook.com>
2023-02-05 15:38:55 -0300

    Increment ModalModule

--- src/modules/Modal/components/ModalModule.jsx
+++ src/modules/Modal/components/ModalModule.jsx
@@ -1,19 +1,43 @@
 import React from "react";
 import { MODAL_CONTENT_TYPES } from "constants/modal";
 
-import PartnershipsModalContent from "modules/Modal/modalSubmodules/PartnershipsModalContent";
-import ResalesModalContent from "modules/Modal/modalSubmodules/ResalesModalContent";
-import ExportsModalContent from "modules/Modal/modalSubmodules/ExportsModalContent";
-import ProductDoubtModalContent from "modules/Modal/modalSubmodules/ProductDoubtModalContent";
-import TipOverflowModalContent from "modules/Modal/modalSubmodules/TipOverflowModalContent";
-import TipUnderflowModalContent from "modules/Modal/modalSubmodules/TipUnderflowModalContent";
 import CancelledOrderModalContent from "modules/Modal/modalSubmodules/CancelledOrderModalContent";
-import ProductCompositionModalContent from "modules/Modal/modalSubmodules/ProductCompositionModalContent";
-import OrderDoubtModalContent from "modules/Modal/modalSubmodules/OrderDoubtModalContent";
+import ChangeShippingAddressModalContent from "modules/Modal/modalSubmodules/ChangeShippingAddressModalContent";
+import DeliveryDeadlinesModalContent from "modules/Modal/modalSubmodules/DeliveryDeadlinesModalContent";
+import ExportsModalContent from "modules/Modal/modalSubmodules/ExportsModalContent";
+import LateOrderModalContent from "modules/Modal/modalSubmodules/LateOrderModalContent";
 import NonCustomerProductDoubtModalContent from "modules/Modal/modalSubmodules/NonCustomerProductDoubtModalContent";
+import OrderDoubtModalContent from "modules/Modal/modalSubmodules/OrderDoubtModalContent";
+import OrderStatusModalContent from "modules/Modal/modalSubmodules/OrderStatusModalContent";
+import OrderTrackingModalContent from "modules/Modal/modalSubmodules/OrderTrackingModalContent";
+import PartnershipsModalContent from "modules/Modal/modalSubmodules/PartnershipsModalContent";
+import ProductCompositionModalContent from "modules/Modal/modalSubmodules/ProductCompositionModalContent";
+import ProductDoubtModalContent from "modules/Modal/modalSubmodules/ProductDoubtModalContent";
+import ProductReturnModalContent from "modules/Modal/modalSubmodules/ProductReturnModalContent";
+import ResalesModalContent from "modules/Modal/modalSubmodules/ResalesModalContent";
+import TipUnderflowModalContent from "modules/Modal/modalSubmodules/TipUnderflowModalContent";
+import TipOverflowModalContent from "modules/Modal/modalSubmodules/TipOverflowModalContent";
 
 
 const ModalModule = ({ contentType }) => {
+  if (contentType === MODAL_CONTENT_TYPES.ORDER_STATUS) {
+    return <OrderStatusModalContent />;
+  }
+  if (contentType === MODAL_CONTENT_TYPES.ORDER_TRACKING) {
+    return <OrderTrackingModalContent />;
+  }
+  if (contentType === MODAL_CONTENT_TYPES.DELIVERY_DEADLINES) {
+    return <DeliveryDeadlinesModalContent />;
+  }
+  if (contentType === MODAL_CONTENT_TYPES.LATE_ORDER) {
+    return <LateOrderModalContent />;
+  }
+  if (contentType === MODAL_CONTENT_TYPES.PRODUCT_RETURN) {
+    return <ProductReturnModalContent />;
+  }
+  if (contentType === MODAL_CONTENT_TYPES.CHANGE_SHIPPING_ADDRESS) {
+    return <ChangeShippingAddressModalContent />;
+  }
   if (contentType === MODAL_CONTENT_TYPES.PARTNERSHIPS) {
     return <PartnershipsModalContent />;
   }
