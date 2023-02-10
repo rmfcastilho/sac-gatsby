38ee7a7f662f627fcbb06ae3ae83fde8fabe4668
Rodrigo M F Castilho <rodrigo.m.castilho@outlook.com>
2023-02-10 18:41:36 -0300

    Add validators

--- src/modules/Forms/components/AddressForm/AddressForm.component.jsx
+++ src/modules/Forms/components/AddressForm/AddressForm.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from "react-redux";
 
 import { updateAddressForm } from "slices/formSlices/addressForm.slice";
+import { composeValidators } from "modules/Forms/helpers/composeValidators";
 
 import {
   ADDRESS_FORM_HEADER,
@@ -18,6 +19,8 @@ import {
   LoneStyledFieldNarrow
 } from "modules/Forms/styles/Form.styles";
 
+import { requiredValidator, isNumberValidator } from "modules/Forms/helpers/fieldValidation";
+
 
 const AddressForm = () => {
   const dispatch = useDispatch();
@@ -27,7 +30,7 @@ const AddressForm = () => {
       name: e.target.name,
       value: e.target.value,
     })
-  )
+  );
 
   return (
     <FormSubsection onChange={handleChange}>
@@ -39,41 +42,50 @@ const AddressForm = () => {
         name={ADDRESS_FORM_FIELDS.ZIP}
         component="input"
         placeholder={ADDRESS_FORM_LABELS[ADDRESS_FORM_FIELDS.ZIP]}
+        maxLength={9}
+        validate={composeValidators(requiredValidator, isNumberValidator)}
+        type="number"
       />
 
       <StyledFieldWide
         name={ADDRESS_FORM_FIELDS.STREET}
         component="input"
         placeholder={ADDRESS_FORM_LABELS[ADDRESS_FORM_FIELDS.STREET]}
+        validate={requiredValidator}
       />
       <StyledFieldNarrow
         name={ADDRESS_FORM_FIELDS.NUMBER}
         component="input"
         placeholder={ADDRESS_FORM_LABELS[ADDRESS_FORM_FIELDS.NUMBER]}
+        validate={composeValidators(requiredValidator, isNumberValidator)}
       />
 
       <StyledFieldFullWidth
         name={ADDRESS_FORM_FIELDS.COMPLEMENT}
         component="input"
         placeholder={ADDRESS_FORM_LABELS[ADDRESS_FORM_FIELDS.COMPLEMENT]}
+        validate={requiredValidator}
       />
 
       <StyledFieldFullWidth
         name={ADDRESS_FORM_FIELDS.DISTRICT}
         component="input"
         placeholder={ADDRESS_FORM_LABELS[ADDRESS_FORM_FIELDS.DISTRICT]}
+        validate={requiredValidator}
       />
 
       <StyledFieldWide
         name={ADDRESS_FORM_FIELDS.CITY}
         component="input"
         placeholder={ADDRESS_FORM_LABELS[ADDRESS_FORM_FIELDS.CITY]}
+        validate={requiredValidator}
       />
 
       <StyledFieldNarrow
         name={ADDRESS_FORM_FIELDS.STATE}
         component="input"
         placeholder={ADDRESS_FORM_LABELS[ADDRESS_FORM_FIELDS.STATE]}
+        validate={requiredValidator}
       />
     </FormSubsection>
   );
---
+++ src/modules/Forms/helpers/fieldValidation.js
@@ -0,0 +1,50 @@
+export const requiredValidator = (value) => {
+  if (!value) {
+    return 'Campo obrigatório';
+  }
+
+  return undefined;
+}
+
+export const maxLengthValidator = (max) => (value) => {
+  if (value && value.length > max) {
+    return `Máximo de ${max} caracteres`;
+  }
+
+  return undefined;
+};
+
+export const minLengthValidator = (min) => (value) => {
+  if (value && value.length < min) {
+    return `Mínimo de ${min} caracteres`;
+  }
+
+  return undefined;
+}
+
+export const isNumberValidator = (value) => {
+  if (value && isNaN(Number(value))) {
+    return 'Deve ser um número';
+  }
+
+  return undefined;
+}
+
+export const isEmailValidValidator = (value) => {
+  if (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    return 'Deve ser um e-mail válido';
+  }
+
+  return undefined;
+}
+
+export const isCpfValidValidator = (value) => {
+  if (value && !/^\d{3}\.\d{3}\.\d{3}-\d{2}$/i.test(value)) {
+    return 'Deve ser um CPF válido';
+  }
+
+  return undefined;
+}
+
+
+
