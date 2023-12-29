267633ff074eb168a380f72994799337b8043e10
castilhor <rodrigo.m.castilho@outlook.com>
2023-12-29 13:20:53 -0300

    cors troubleshooting

--- src/api/submitNewRequest.js
+++ src/api/submitNewRequest.js
@@ -1,6 +1,6 @@
 export const submitNewRequest = async (type, data) => {
   try {
-    const response = await fetch(`${process.env.GATSBY_API_BASE_URL}`, {
+    const response = await fetch(`${process.env.GATSBY_API_BASE_URL}/submit-new-request`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -9,11 +9,19 @@ export const submitNewRequest = async (type, data) => {
       body: JSON.stringify(data),
     });
 
+    console.log('Response Status:', response.status);
+    console.log('Response Headers:', response.headers);
+
     if (!response.ok) {
+      const errorResponse = await response.json();
+      console.error('Error Response:', errorResponse);
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    return await response.json();
+    const responseData = await response.json();
+    console.log('Response Data:', responseData);
+
+    return responseData;
   } catch (error) {
     console.error('Error:', error);
     return error;
